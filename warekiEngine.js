9b64194bb68d8bf1c57d4385e3bd6c1f6fe69d6a
MichinariNukazawa <michinari.nukazawa@gmail.com>
2021-09-06 06:10:52 +0900

    内部的に分割されている和暦文字列の変換に対応

--- warekiEngine.js
+++ warekiEngine.js
@@ -12,14 +12,80 @@ dictionary.set('皇紀', {'conv':(num)=>{return num - 660;}});
 
 const warekiMap = dictionary;
 
+const rsRekiStrNum = `元年|\\d+年?|[${wideNumAllChars}]+年?`;
 let regexs = new Map();
 for (let word of warekiMap.keys()) {
-	regexs.set(word, new RegExp(`(${word}\s*)(元年|\\d+年?|[${wideNumAllChars}]+年?)(.{0,10})?`, 'gi'));
+	regexs.set(word, new RegExp(`(${word}\s*)(${rsRekiStrNum})(.{0,10})?`, 'gi'));
+}
+
+function genPrintString(wareki, sHead, sNum, sExt){
+	let num = 0;
+	if('元年' === sNum){
+		num = 1;
+	}else if(-1 !== zenkakuArabicNumChars.indexOf(sNum[0])){
+		num = convZenkakuStr2Int(sNum);
+		if(isNaN(num)){
+			// Warn: can not convert.
+			return `${sHead}<?'${sNum}'>${sExt}`;
+		}
+	}else if(-1 !== kanSuuziAllChars.indexOf(sNum[0])){
+		if(rKanSuuziKeta.test(sNum)){
+			num = convKanSuuziWithKetaStr2Int(sNum);
+		}else{
+			num = convKanSuuziStr2Int(sNum);
+		}
+		if(isNaN(num)){
+			// Warn: can not convert.
+			return `${sHead}<?'${sNum}'>${sExt}`;
+		}
+	}else{
+		num = parseInt(sNum, 10);
+		if(isNaN(num)){
+			// Warn: can not convert.
+			return `${sHead}<?'${sNum}'>${sExt}`;
+		}
+	}
+	const seireki = wareki.conv(num);
+	let sSeireki = `${seireki}`;
+	if(0 >= seireki){
+		// https://ja.wikipedia.org/wiki/西暦#0と負の西暦
+		sSeireki = `BCE${Math.abs(seireki) + 1}`;
+	}
+	return `${sHead}${sNum}<${sSeireki}>${sExt}`;
+}
+
+function getLastPrevText(node)
+{
+	if(null === node){
+		return '';
+	}
+
+	const prevNode = node.previousSibling;
+	if(null === prevNode){
+		return getLastPrevText(node.parentNode);
+	}
+	if(null === prevNode.textContent){
+		return getLastPrevText(prevNode);
+	}
+	if(/^\s*$/.test(prevNode.textContent)){ // 必要なのかわからないがとりあえず
+		return getLastPrevText(prevNode);
+	}
+	return prevNode.textContent;
 }
 
 const convWareki = (node) => {
 	let content = node.textContent;
 	for (let [word, wareki] of warekiMap) {
+		// 直前のnodeの末尾と現在のnodeの先頭を連結して、和暦になるようであれば変換を行う
+		const prevText = getLastPrevText(node);
+		if(prevText.endsWith(word)){
+			const rRekiStrNum = new RegExp(`^(${rsRekiStrNum})`);
+			content = content.replace(rRekiStrNum, (match, sNum) => {
+				return genPrintString(wareki, '', sNum, '');
+			});
+		}
+
+		// node内の和暦に対して変換を行う
 		const regex = regexs.get(word);
 		content = content.replace(regex, (match, sHead, sNum, sExt) => {
 			// TODO can not able check splited text other node(prev node).
@@ -48,39 +114,7 @@ const convWareki = (node) => {
 			}
 
 			sExt = (sExt)? sExt : '';
-			let num = 0;
-			if('元年' === sNum){
-				num = 1;
-			}else if(-1 !== zenkakuArabicNumChars.indexOf(sNum[0])){
-				num = convZenkakuStr2Int(sNum);
-				if(isNaN(num)){
-					// Warn: can not convert.
-					return `${sHead}<?'${sNum}'>${sExt}`;
-				}
-			}else if(-1 !== kanSuuziAllChars.indexOf(sNum[0])){
-				if(rKanSuuziKeta.test(sNum)){
-					num = convKanSuuziWithKetaStr2Int(sNum);
-				}else{
-					num = convKanSuuziStr2Int(sNum);
-				}
-				if(isNaN(num)){
-					// Warn: can not convert.
-					return `${sHead}<?'${sNum}'>${sExt}`;
-				}
-			}else{
-				num = parseInt(sNum, 10);
-				if(isNaN(num)){
-					// Warn: can not convert.
-					return `${sHead}<?'${sNum}'>${sExt}`;
-				}
-			}
-			const seireki = wareki.conv(num);
-			let sSeireki = `${seireki}`;
-			if(0 >= seireki){
-				// https://ja.wikipedia.org/wiki/西暦#0と負の西暦
-				sSeireki = `BCE${Math.abs(seireki) + 1}`;
-			}
-			return `${sHead}${sNum}<${sSeireki}>${sExt}`;
+			return genPrintString(wareki, sHead, sNum, sExt);
 		});
 	}
 
